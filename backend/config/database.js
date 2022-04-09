f4345cf8c6e4a9ea7d8711dc08a0996d2e1193ee
Emuallim <muhiddinjv@gmail.com>
2022-04-10 00:15:56 +0500

    chore: connect localhost to mongodb

---
+++ backend/config/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI)
+    .then(con => {console.log(`Mongo DB connected with HOST: ${con.connection.host}`)})
+}
+
+module.exports = connectDatabase
+
+//* important info
+//! old method be careful
+//? is it really necessary?
+//TODO: refactor this part
+//@param myParam
+//MORE THAN 3 SLASHES = ////CROSS OUT
+
+/* 
+TODO: 1) add DB_LOCAL_URI = mongodb://localhost:27017/shopit/ to config.env DONE
+TODO: 2) connect mongoose thru DB_LOCAL_URI with process.env DONE
+TODO: 3) pass object to connect and set useNewUrlParse, useUnifiedTopology, useCreateIndex to true in obj DONE
+TODO: 4) .then in arrow func, clg MONGO DB connected with HOST: con.connection.host DONE
+TODO: 5) import it in sever.js DONE
+*/
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 // setting up config file
 dotenv.config({path:'backend/config/config.env'})
+
+// connecting to database
+connectDatabase();
 
 const { PORT, NODE_ENV } = process.env;
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port: ${PORT} in ${NODE_ENV} mode`)
 })
+
+/*
+TODO: 0) Look in database.js first DONE
+TODO: 1) import database named connectDatabase DONE
+TODO: 2) Call the connectDatabase func here DONE
+*/
