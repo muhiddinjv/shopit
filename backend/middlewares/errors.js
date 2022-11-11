3a55b6f89a30d02f372fcfb8e327589d422c253d
Muhiddin <muhiddinjv@gmail.com>
2022-11-11 09:14:05 +0500

    feat: handle product validation errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -22,6 +22,12 @@ const errorMiddleware = async (err, req, res, next) => {
       error = new ErrorHandler(message, 400);
     }
 
+    // Handle mongoose validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Server Error",
