f951d9123e2db7a63aeae9eec073ba270c32070d
Muhiddin <muhiddinjv@gmail.com>
2022-12-18 22:41:52 +0500

    bug: range picker not working

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
-import MetaData from "./layout/MetaData";
 import Pagination from "react-js-pagination";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+import MetaData from "./layout/MetaData";
+import Product from "./product/Product";
+import Loader from "./layout/Loader";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
-import Product from "./product/Product";
-import Loader from "./layout/Loader";
 import { useAlert } from "react-alert";
 import { useParams } from "react-router-dom";
 
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range);
+
 const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [price, stPrice] = useState([1, 1000]);
+  const [price, setPrice] = useState([1, 1000]);
 
   const alert = useAlert();
   const params = useParams();
@@ -20,14 +26,16 @@ const Home = () => {
     (state) => state.products
   );
 
+  const keyword = params.keyword;
+
   useEffect(() => {
     if (error) {
       alert.success("Success!");
       return alert.error(error);
     }
 
-    dispatch(getProducts(params.keyword, currentPage, price));
-  }, [dispatch, alert, error, params.keyword, currentPage, price]);
+    dispatch(getProducts(keyword, currentPage, price));
+  }, [dispatch, alert, error, keyword, currentPage, price]);
 
   function setCurrentPageNo(pageNumber) {
     setCurrentPage(pageNumber);
@@ -43,10 +51,32 @@ const Home = () => {
           <h1 id="products_heading">Latest Products</h1>
           <section id="products" className="container mt-5">
             <div className="row">
-              {products &&
+              {keyword ? (
+                <div className="col-6 col-md-3 mt-5 mb-5">
+                  <div className="px-5">
+                    <Range
+                      mark={{
+                        1: "$1",
+                        1000: "$1000",
+                      }}
+                      min={1}
+                      max={1000}
+                      defaultValue={[1, 1000]}
+                      tipFormatter={(value) => `$${value}`}
+                      tipProps={{
+                        placement: "top",
+                        visible: true,
+                      }}
+                      value={price}
+                      onChange={(price) => setPrice(price)}
+                    />
+                  </div>
+                </div>
+              ) : (
                 products.map((product) => (
                   <Product key={product._id} product={product} />
-                ))}
+                ))
+              )}
             </div>
           </section>
           {resPerPage <= productsCount && (
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Search from "./Search";
-import { Route, Routes } from "react-router-dom";
 
 const Header = () => {
   return (
