1cdc35d561a5e6f0d90c263fd6b5af6e3c39cc39
Muhiddin <muhiddinjv@gmail.com>
2023-01-03 16:25:41 +0500

    feat: add register user

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import MetaData from "../layout/MetaData";
 import Loader from "../../components/layout/Loader";
